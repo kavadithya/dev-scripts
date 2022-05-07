59c0257d1c326a479d37480ec8debd417ae921a0
Kavuluru Adithya <kav.adithya@gmail.com>
2022-05-07 16:44:44 +0530

    print all missing env vars names

--- env_vars.sh
+++ env_vars.sh
@@ -2,14 +2,23 @@
 
 check_env_vars() {
     ENV_VARS="$1"
-    echo "checking for env vars: $ENV_VARS"
+    ALL_GOOD=1
+
+    echo "checking for required env vars"
+
     ## check that all required environment variables are present
     for name in "${ENV_VARS[@]}"; do
         if [[ -z "${!name+x}" ]]; then
             echo "Variable $name not set!"
-            exit 1
+            ALL_GOOD=0
         fi
     done
+
+    ## exit 1 if not all vars are present
+    if [[ "$ALL_GOOD" -eq 0 ]]; then
+        echo "some required variables missing - exit 1";
+        exit 1
+    fi
 
     echo "following environment variables are present: ${ENV_VARS[@]}"
 }
